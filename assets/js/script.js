bf7fa0c361504b3bdd2c132a5dc36c25d6563dae
christianrueda <81277977+imchrisrueda@users.noreply.github.com>
2025-03-28 19:05:40 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,3 +39,39 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+// Cargar artículos
+document.addEventListener('DOMContentLoaded', () => {
+  renderArticulos(); // Usa el selector por defecto
+});
+function renderArticulos(selector = '#articulos') {
+  const container = document.querySelector(selector);
+  if (!container) return;
+
+  const articulos = container.querySelectorAll('.articulo');
+
+  articulos.forEach(div => {
+    const title = div.dataset.title;
+    const published = div.dataset.published;
+    const summary = div.dataset.summary;
+    const link = div.dataset.link;
+
+    div.innerHTML = `
+      <div class="flex items-center justify-between">
+        <a href="${link}" target="_blank" class="articulo-titulo hover:underline">
+          ${title}
+        </a>
+        <a href="${link}" target="_blank" class="text-gray-400 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400" aria-label="Ver artículo">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M14 3h7m0 0v7m0-7L10 14m-4 0h4v4" />
+          </svg>
+        </a>
+      </div>
+      <p class="text-sm text-gray-600 dark:text-gray-300">${published}</p>
+      <p class="mt-2 text-gray-800 dark:text-gray-300">${summary}</p>
+    
+  `;
+  
+  });
+}
